9e603ffa438df3c34a8565c7b1fe6dc424083b11
Alfaza Satria <alfazasatria8@gmail.com>
2020-04-09 18:36:17 +0700

    Add Blogpost

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import { reducer as formReducer } from 'redux-form';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-const rootReducer = combineReducers({
-    form: formReducer,
-});
-const store = createStore(rootReducer);
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
-registerServiceWorker();
+import * as serviceWorker from './serviceWorker';
+
+import BlogPost from "./container/BlogPost/BlogPost";
+
+ReactDOM.render(<BlogPost />, document.getElementById('content'));
+
+serviceWorker.unregister();
